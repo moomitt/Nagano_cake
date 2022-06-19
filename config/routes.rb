dc4e1fee9d2e8660fbc082c6953e5204aca92dd2
moomitt <100453545+moomitt@users.noreply.github.com>
2022-06-19 02:35:11 +0000

    [3.1.0] admin用コントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-
-  namespace :admin do
-    root to: 'homes#top'
-  end
+  
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
@@ -11,6 +8,16 @@ Rails.application.routes.draw do
   devise_for :admins, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+  
+  namespace :admin do
+    root to: 'homes#top'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :cusromers, only: [:index, :create, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+  end
+  
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
