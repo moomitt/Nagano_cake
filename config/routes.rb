4b7bc347c5e7172ebb78822b29c10746efba193e
moomitt <100453545+moomitt@users.noreply.github.com>
2022-06-25 11:29:55 +0000

    [3.2.0] ECサイト：ルーティング・ヘッダー再修正

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     root to: 'homes#top'
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
-    resources :customers, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :create, :edit, :update]
     resources :orders, only: [:show, :update]
     resources :order_details, only: [:update]
   end
