db4398feec8f8a10d0d149ae74efec10d40a8d05
moomitt <100453545+moomitt@users.noreply.github.com>
2022-07-03 05:50:49 +0000

    [3.2.7] ECサイト：退会ページ・退会機能実装

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -16,6 +16,13 @@ class Public::CustomersController < ApplicationController
   def confirm
   end
   
+  def withdraw
+    customer = current_customer
+    customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+  
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana,
     :first_name_kana, :postal_code, :address, :telephone_number, :email)
