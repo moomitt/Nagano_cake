8a4133c00d33a8e48c162ae835e2e0aeccffb185
moomitt <100453545+moomitt@users.noreply.github.com>
2022-07-03 08:56:24 +0000

    [3.2.9] ECサイト：配送先編集画面

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -12,6 +12,13 @@ class Public::AddressesController < ApplicationController
   end
 
   def edit
+    @address = Address.find(params[:id])
+  end
+  
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path
   end
   
   def destroy
