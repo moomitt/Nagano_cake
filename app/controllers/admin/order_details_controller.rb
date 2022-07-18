1296714ff5afa22bc58b1a73cdb6f50e4b7de2be
moomitt <100453545+moomitt@users.noreply.github.com>
2022-07-18 11:13:07 +0000

    [3.1.12] 注文履歴詳細画＋エラー微修正

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,20 @@
 class Admin::OrderDetailsController < ApplicationController
+  def update
+    @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    @order_detail.update(order_detail_params)
+    if params[:order_detail][:making_status] == "making"
+      @order.update(status: "making")
+    end
+    if @order.order_details.count == @order.order_details.where(making_status: "finish").count
+      @order.update(status: "preparing_delivery")
+    end
+    redirect_to admin_order_path(@order.id)
+  end
+  
+  private
+  def order_detail_params
+    params.require(:order_detail).permit(:making_status)
+  end
+  
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,22 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: @order.id)
   end
+  
+  def update
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: @order.id)
+    @order.update(order_params)
+    if @order.status == "confirm_payment"
+      @order_details.update_all(making_status: "wait_making")
+    end
+    redirect_to admin_order_path(@order.id)
+  end
+  
+  private
+  def order_params
+    params.require(:order).permit(:status)
+  end
+
 end
